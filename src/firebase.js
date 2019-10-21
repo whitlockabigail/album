5b9e6556dd93ebf6851a8e85945038af966f7bb9
whitlockabigail <55110985+whitlockabigail@users.noreply.github.com>
2019-10-21 14:37:36 -0600

    Firebase and Travis setup

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import "firebase/auth";
 import "firebase/firestore";
 
 const config = {
-  apiKey: "AIzaSyAfsWPucjkyc9Q1NWgyzB7n3x7o6-rtdIw",
-  authDomain: "auth-d3325.firebaseapp.com",
-  databaseURL: "https://auth-d3325.firebaseio.com",
-  projectId: "auth-d3325",
-  storageBucket: "",
-  messagingSenderId: "745798849549",
-  appId: "1:745798849549:web:157fb67e31f59e182db144"
+  apiKey: "AIzaSyBZd7Oj5hXLH2EvLoUgpbXyhyqxmGUfACU",
+  authDomain: "album-6300a.firebaseapp.com",
+  databaseURL: "https://album-6300a.firebaseio.com",
+  projectId: "album-6300a",
+  storageBucket: "album-6300a.appspot.com",
+  messagingSenderId: "971308488451",
+  appId: "1:971308488451:web:4261fb10376453c0e87b36"
 };
 
 firebase.initializeApp(config);
